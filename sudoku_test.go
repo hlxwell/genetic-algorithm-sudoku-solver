85bca1b774762155c53bcc92e63d47feeacb667f
Michael He <michael.he@bytedance.com>
2021-05-31 04:13:45 +0900

    Add test for validate box count

--- sudoku_test.go
+++ sudoku_test.go
@@ -8,7 +8,7 @@ var matrix = []string{
 	"W", "R", "O", "D",
 	"D", "O", "W", "R",
 	"O", "D", "R", "W",
-	"R", "W", "D", "O",
+	"R", "W", "D", "D",
 }
 
 func TestColumns(t *testing.T) {
@@ -29,5 +29,12 @@ func TestBoxes(t *testing.T) {
 	s := NewSudoku(matrix)
 	if len(s.Rows()) != s.EdgeLength {
 		t.Errorf("Boxes count should be %d, but was %d", s.EdgeLength, len(s.Boxes()))
+	}
+}
+
+func TestValidSolutionCount(t *testing.T) {
+	s := NewSudoku(matrix)
+	if s.ValidSolutionCount() != 9 {
+		t.Errorf("ValidSolutionCount should be %d, but was %d", 9, s.ValidSolutionCount())
 	}
 }
