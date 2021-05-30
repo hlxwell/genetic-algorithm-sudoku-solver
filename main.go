6ffcc8aea61fbedc35a686b7aad0d85f78eb7029
Michael He <michael.he@bytedance.com>
2021-05-31 04:13:58 +0900

    Fix main.

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
-import "fmt"
-
 func main() {
-	// sudoku := []string{
-	// 	"W", "R", "O", "D",
-	// 	"D", "O", "W", "R",
-	// 	"O", "D", "R", "W",
-	// 	"R", "W", "D", "O",
-	// }
+	c := NewChromosome(10000)
+	c.GeneratePopulation()
+	c.Evolve()
 
-	c := NewChromosome(10)
-	for i := 0; i < 100; i++ {
-		fmt.Println(c.Generate())
-	}
+	c.CurrentBestSolution.PrettyPrint()
 }
